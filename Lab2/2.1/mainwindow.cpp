64748332b45c5178f0bc729e95d7d162ea8745a6
Tenebris <56090719+Tenebraeth@users.noreply.github.com>
2022-06-02 21:32:55 +0200

    Add files via upload

---
+++ Lab2/2.1/mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPainter>
+#include <QMouseEvent>
+#include <point.h>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    for (int i=0;i<n;i++){
+        QPen pen;
+        pen.setWidth(1);
+        painter.setPen(pen);
+        painter.drawLine(points[i]->getX()-3,points[i]->getY(),points[i]->getX()+3,points[i]->getY());
+        painter.drawLine(points[i]->getX(),points[i]->getY()-3,points[i]->getX(),points[i]->getY()+3);
+    }
+
+
+
+
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    Point *point = nullptr;
+    if (n>=10) return;
+
+    point = new Point(event->x(),event->y());
+    if(point)
+        points[n++] = point;
+
+    repaint();
+}
+
